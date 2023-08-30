744d6608c50a48123b8479f27c183fe8fa0c86ac
Charan <charankulal0241@gmail.com>
2023-08-30 12:21:06 +0530

    Dynamically fetching data from array using map function

--- DAY 10(AUG 30)/sampleapp/src/Components/Card.js
+++ DAY 10(AUG 30)/sampleapp/src/Components/Card.js
@@ -1,20 +1,19 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
   return (
     
       <div className="col-md-4 d-flex" >
               <div className="card mb-4 shadow-sm">
                 <img
+                src={props.imageUrl}
                   className="card-img-top"
-                  src={require("./Thumbnail.jpg")}
+                  
                   alt="Thumbnail"
                 />
                 <div className="card-body">
                   <p className="card-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-                    soluta, explicabo molestias pariatur reprehenderit eligendi
-                    placeat maxime adipisci hic rem nemo officia ab libero!
+                    {props.description}
                   </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
@@ -31,7 +30,7 @@ export default function Card() {
                         Edit
                       </button>
                     </div>
-                    <small className="text-muted">9 mins</small>
+                    <small className="text-muted">{props.time}</small>
                   </div>
                 </div>
               </div>
