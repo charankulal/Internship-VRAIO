efed489ea8e5cdc99064e13589fecaa71edd86fe
Charan <charankulal0241@gmail.com>
2023-08-30 10:54:08 +0530

    Making a Component

---
+++ DAY 9(AUG 29)/sampleapp/src/Components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+        <h1 className="jumbotron-heading p-5">Album example</h1>
+        <p className="lead text-muted" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          libero aspernatur exercitationem alias quo maiores magni corporis
+          distinctio sint in impedit, sapiente incidunt quaerat?
+        </p>
+        <p>
+          <button className="btn btn-primary my-2 mx-3">Main call to action</button>
+          <button className="btn btn-secondary my-2">Secondary action</button>
+        </p>
+      </div>
+      
+    )
+  }
+}
+
+export default Header
