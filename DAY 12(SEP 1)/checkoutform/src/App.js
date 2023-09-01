72f2a90206d4ce2a772d41151107be7c042bac8e
Charan <charankulal0241@gmail.com>
2023-09-01 10:30:27 +0530

    Added Footer Component

--- DAY 12(SEP 1)/checkoutform/src/App.js
+++ DAY 12(SEP 1)/checkoutform/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Heading from './Components/Heading';
 import Cart from './Components/Cart';
 import Billing from './Components/Billing';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -13,14 +14,7 @@ function App() {
         <Cart/>
         <Billing/>
       </div>
-      <footer className="my-5 pt-5 text-muted text-center text-small">
-        <p className="mb-1">&copy; 2017-2018 Company Name</p>
-        <ul className="list-inline">
-          <li className="list-inline-item"><a href="/">Privacy</a></li>
-          <li className="list-inline-item"><a href="/">Terms</a></li>
-          <li className="list-inline-item"><a href="/">Support</a></li>
-        </ul>
-      </footer>
+      <Footer/>
 </div>
 
       
