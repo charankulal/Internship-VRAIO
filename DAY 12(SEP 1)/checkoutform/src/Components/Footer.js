3be7b15ed87c01791ca5b393753a9a4295b35de3
Charan <charankulal0241@gmail.com>
2023-09-01 10:42:50 +0530

    Created Li Component

--- DAY 12(SEP 1)/checkoutform/src/Components/Footer.js
+++ DAY 12(SEP 1)/checkoutform/src/Components/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import Li from './Li'
 
 export default function Footer() {
+    const data=["Privacy","Terms","Support"]
   return (
     
       <footer className="my-5 pt-5 text-muted text-center text-small">
         <p className="mb-1">&copy; 2017-2018 Company Name</p>
         <ul className="list-inline">
-          <li className="list-inline-item"><a href="/">Privacy</a></li>
-          <li className="list-inline-item"><a href="/">Terms</a></li>
-          <li className="list-inline-item"><a href="/">Support</a></li>
+            {data.map((ele)=>{
+                return(
+                    <Li desc={ele}/>
+                );
+            })}
+          
         </ul>
       </footer>
     
---
+++ DAY 12(SEP 1)/checkoutform/src/Components/Li.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Li(props) {
+  return (
+    <li className="list-inline-item"><a href="/">{props.desc}</a></li>
+  )
+}
