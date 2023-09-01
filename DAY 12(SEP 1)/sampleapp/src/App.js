94fc723db187a80f1dfc42ac43ff4a568bb28ac6
Charan <charankulal0241@gmail.com>
2023-09-01 12:13:28 +0530

    Created new app for sculptureList

---
+++ DAY 12(SEP 1)/sampleapp/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+  var [index, setIndex] = useState(0);
+
+  function handleClick() {
+    if(index===11)
+    {
+      index=-1
+      setIndex(index + 1)
+    }
+    else{
+      setIndex(index + 1);
+    }
+
+    
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index} of {sculptureList.length})
+      </h3>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+    </>
+  );
+}
