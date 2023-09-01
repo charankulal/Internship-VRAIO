6ce82ee99b348fc3445a06f6c3f792444cb95b10
Charan <charankulal0241@gmail.com>
2023-09-01 11:01:32 +0530

    Created Payment component

--- DAY 12(SEP 1)/checkoutform/src/Components/Billing.js
+++ DAY 12(SEP 1)/checkoutform/src/Components/Billing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Payment from './Payment'
 
 export default function Billing() {
   return (
@@ -140,96 +141,7 @@ export default function Billing() {
             </div>
             <hr className="mb-4" />
 
-            <h4 className="mb-3">Payment</h4>
-
-            <div className="d-block my-3">
-              <div className="custom-control custom-radio">
-                <input
-                  id="credit"
-                  name="paymentMethod"
-                  type="radio"
-                  className="custom-control-input"
-                  defaultChecked
-                  required
-                />
-                <label className="custom-control-label" htmlFor="credit">Credit card</label>
-              </div>
-              <div className="custom-control custom-radio">
-                <input
-                  id="debit"
-                  name="paymentMethod"
-                  type="radio"
-                  className="custom-control-input"
-                  required
-                />
-                <label className="custom-control-label" htmlFor="debit"
-                  >Debit card</label>
-                
-              </div>
-              <div className="custom-control custom-radio">
-                <input
-                  id="paypal"
-                  name="paymentMethod"
-                  type="radio"
-                  className="custom-control-input"
-                  required
-                />
-                <label className="custom-control-label" htmlFor="paypal">PayPal</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6 mb-3">
-                <label htmlFor="cc-name">Name on card</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="cc-name"
-                  placeholder=""
-                  required
-                />
-                <small className="text-muted">Full name as displayed on card</small>
-                <div className="invalid-feedback">Name on card is required</div>
-              </div>
-              <div className="col-md-6 mb-3">
-                <label htmlFor="cc-number">Credit card number</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="cc-number"
-                  placeholder=""
-                  required
-                />
-                
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-3 mb-3">
-                <label htmlFor="cc-expiration">Expiration</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="cc-expiration"
-                  placeholder=""
-                  required
-                />
-                
-              </div>
-              <div className="col-md-3 mb-3">
-                <label htmlFor="cc-cvv">CVV</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="cc-cvv"
-                  placeholder=""
-                  required
-                />
-                
-              </div>
-            </div>
-            <hr className="mb-4" />
-            <button className="btn btn-primary btn-lg btn-block" type="submit">
-              Continue to checkout
-            </button>
+           <Payment/>
           </form>
         </div>
   )
