c0e7ee855f223959aa549262871f5af04dba9f10
Charan <charankulal0241@gmail.com>
2023-09-03 22:52:27 +0530

    Fixed all Bugs

--- DAY 13(SEP 4)/calculator/src/Components/DigitButton.js
+++ DAY 13(SEP 4)/calculator/src/Components/DigitButton.js
@@ -4,7 +4,7 @@ export default function DigitButton({ dispatch, digit }) {
   return (
     <button
     className="btn btn-outline-primary col-12 m-2"
-      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGITS, payload: { digit } })}
     >
       {digit}
     </button>
