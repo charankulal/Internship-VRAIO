3197b60d1a3bcebae67fdbe263f221ccd5c35b5f
Charan <charankulal0241@gmail.com>
2023-09-06 06:41:17 +0530

    Corrected the alignment

--- DAY 13(SEP 5)/calculator/src/Components/Output.js
+++ DAY 13(SEP 5)/calculator/src/Components/Output.js
@@ -11,52 +11,53 @@ const Output = ({ input, setInput, answer }) => {
 
   return (
     <>
-      
-        {answer === "" ? (
-          <>
+      {answer === "" ? (
+        <>
+          <input
+            type="text"
+            name="input"
+            className="form-control my-2"
+            style={{ fontSize: "2rem" }}
+            value={input}
+            placeholder="0"
+            maxLength={12}
+            onChange={onChangeTagInput}
+            autoComplete="off"
+          />
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            name="input"
+            className="form-control my-2"
+            style={{ fontSize: "2rem" }}
+            value={input}
+            placeholder="0"
+            maxLength={12}
+            disabled
+          />
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span
+                className="input-group-text"
+                style={{ fontSize: "2rem", textAlign: "right", color: "red" }}
+              >
+                Ans
+              </span>
+            </div>
             <input
-              type="text"
-              name="input"
-              className="form-control my-2"
-              style={{ fontSize:"2rem" }}
-              value={input}
-              placeholder="0"
-              maxLength={12}
-              
-              onChange={onChangeTagInput}
-              autoComplete="off"
-            />
-          </>
-        ) : (
-          <>
-            <input
-              type="text"
-              name="input"
-              className="form-control my-2"
-              style={{ fontSize:"2rem" }}
-              value={input}
-              placeholder="0"
-              maxLength={12}
-              disabled
-            />
-            <div className="input-group">
-                <div className="input-group-prepend">
-                  <span className="input-group-text" style={{ fontSize:"2rem",textAlign:"right",color:"red" }} >Ans</span>
-                </div>
-                <input
               type="text"
               name="value"
               className="form-control"
-              style={{ fontSize:"2rem",textAlign:"right",color:"red" }}
-              value={answer==='NaN'?"Syntax Error":answer}
+              style={{ fontSize: "2rem", textAlign: "right", color: "red" }}
+              value={answer === "NaN" ? "Syntax Error" : answer}
               maxLength={12}
               disabled
             />
-              </div>
-            
-          </>
-        )}
-      
+          </div>
+        </>
+      )}
     </>
   );
 };
